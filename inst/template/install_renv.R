b48982f8a73408f01f7becf38b14ad0c7ef25e7b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-01-04 12:05:09 +0000

    Tidy up install scripts

--- inst/template/install_renv.R
+++ inst/template/install_renv.R
@@ -1,4 +1,4 @@
-  ## We can rely here on RENV_ACTIVATE_PROJECT or RENV_AUTOLOADER_ENABLED
+## We can rely here on RENV_ACTIVATE_PROJECT or RENV_AUTOLOADER_ENABLED
 if (Sys.getenv("RENV_AUTOLOADER_ENABLED", "") != "FALSE") {
   stop(
     "Expected environment variable 'RENV_AUTOLOADER_ENABLED' to be 'FALSE'")
--- inst/template/install_script.R
+++ inst/template/install_script.R
@@ -1,12 +1,3 @@
-## We need a CRAN mirror set or nothing works. The user is free to
-## replace this with something else if they want within their script,
-## but this saves every script needing one.
 options(repos = {{repos}})
 
-message("Logs from your installation script '{{script}}' follow:")
-message()
-message(strrep("-", 79))
-message()
 source("{{script}}", echo = TRUE, max.deparse.length = Inf)
-message()
-message(strrep("-", 79))
